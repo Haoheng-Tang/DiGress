52e4e272c004227cd2159b2c7d2266655c60dca1
Haoheng-Tang <haoheng_1209@outlook.com>
2026-04-22 06:21:49 +0000

    Update inpatient_dataset.py

--- src/datasets/inpatient_dataset.py
+++ src/datasets/inpatient_dataset.py
@@ -9,7 +9,7 @@ from torch_geometric.data import InMemoryDataset, download_url
 from src.datasets.abstract_dataset import AbstractDataModule, AbstractDatasetInfos
 
 
-class SpectreGraphDataset(InMemoryDataset):
+class InpatientGraphDataset(InMemoryDataset):
     def __init__(self, dataset_name, split, root, transform=None, pre_transform=None, pre_filter=None):
         self.sbm_file = 'sbm_200.pt'
         self.planar_file = 'planar_64_200.pt'
@@ -32,48 +32,7 @@ class SpectreGraphDataset(InMemoryDataset):
         """
         Download raw qm9 files. Taken from PyG QM9 class
         """
-        if self.dataset_name == 'sbm':
-            raw_url = 'https://raw.githubusercontent.com/KarolisMart/SPECTRE/main/data/sbm_200.pt'
-        elif self.dataset_name == 'planar':
-            raw_url = 'https://raw.githubusercontent.com/KarolisMart/SPECTRE/main/data/planar_64_200.pt'
-        elif self.dataset_name == 'comm20':
-            raw_url = 'https://raw.githubusercontent.com/KarolisMart/SPECTRE/main/data/community_12_21_100.pt'
-        else:
-            raise ValueError(f'Unknown dataset {self.dataset_name}')
-        file_path = download_url(raw_url, self.raw_dir)
-
-        adjs, eigvals, eigvecs, n_nodes, max_eigval, min_eigval, same_sample, n_max = torch.load(file_path)
-
-        g_cpu = torch.Generator()
-        g_cpu.manual_seed(0)
-
-        test_len = int(round(self.num_graphs * 0.2))
-        train_len = int(round((self.num_graphs - test_len) * 0.8))
-        val_len = self.num_graphs - train_len - test_len
-        indices = torch.randperm(self.num_graphs, generator=g_cpu)
-        print(f'Dataset sizes: train {train_len}, val {val_len}, test {test_len}')
-        train_indices = indices[:train_len]
-        val_indices = indices[train_len:train_len + val_len]
-        test_indices = indices[train_len + val_len:]
-
-        train_data = []
-        val_data = []
-        test_data = []
-
-        for i, adj in enumerate(adjs):
-            if i in train_indices:
-                train_data.append(adj)
-            elif i in val_indices:
-                val_data.append(adj)
-            elif i in test_indices:
-                test_data.append(adj)
-            else:
-                raise ValueError(f'Index {i} not in any split')
-
-        torch.save(train_data, self.raw_paths[0])
-        torch.save(val_data, self.raw_paths[1])
-        torch.save(test_data, self.raw_paths[2])
-
+        pass
 
     def process(self):
         file_idx = {'train': 0, 'val': 1, 'test': 2}
@@ -102,7 +61,7 @@ class SpectreGraphDataset(InMemoryDataset):
 
 
 
-class SpectreGraphDataModule(AbstractDataModule):
+class InpatientGraphDataModule(AbstractDataModule):
     def __init__(self, cfg, n_graphs=200):
         self.cfg = cfg
         self.datadir = cfg.dataset.datadir
@@ -125,7 +84,7 @@ class SpectreGraphDataModule(AbstractDataModule):
         return self.inner[item]
 
 
-class SpectreDatasetInfos(AbstractDatasetInfos):
+class InpatientDatasetInfos(AbstractDatasetInfos):
     def __init__(self, datamodule, dataset_config):
         self.datamodule = datamodule
         self.name = 'nx_graphs'
